a648ce0ab1c2d3b3edf0adc235e483a4f97af2ad
Samuel Chen <samuel.net@gmail.com>
2013-09-04 15:44:26 +0800

    .

---
+++ cloud/tool.js
@@ -0,0 +1,18 @@
+var tool = ({ 
+dumpError : function dumpError(err) {
+  if (typeof err === 'object') {
+    if (err.message) {
+      console.log('\nMessage: ' + err.message)
+    }
+    if (err.stack) {
+      console.log('\nStacktrace:')
+      console.log('====================')
+      console.log(err.stack);
+    }
+  } else {
+    console.log('dumpError :: argument is not an object');
+  }
+}
+})
+
+module.exports = tool;
